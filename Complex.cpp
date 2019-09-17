346a1db87a6d26a47c880eb3caa61bbeeb3a3553
CharlesFord120 <charlieford130@gmail.com>
2019-09-17 11:37:24 -0600

    I started the implementation of the Complex.cpp

--- Complex.cpp
+++ Complex.cpp
@@ -5,5 +5,85 @@ Complex::Complex()
 	_real = 0.0;
 	_imag = 0.0;
 }
+Complex& Complex::operator=(const Complex& z)
+{
+	_real = z._real;
+	_imag = z._imag;
+	return *this;
+}
+Complex& Complex::operator=(double r)
+{
+}
+Complex& Complex::operator+=(const Complex& z)
+{
+	_real += z._real;
+	_imag += z._imag;
+	return *this;
+}
+Complex& Complex::operator-=(const Complex& z)
+{
+	_real -= z._real;
+	_imag -= z._imag;
+	return *this;
+}
+Complex& Complex::operator*=(const Complex& z)
+{
+	_real *= z._real;
+	_imag *= z._imag;
+	return *this;
+}
+Complex& Complex::operator/=(const Complex& z)
+{
+	_real /= z._real;
+	_imag /= z._imag;
+	return *this;
+}
 
 
+// basic math operations
+
+Complex operator+(const Complex& a, const Complex& b);
+{
+}
+Complex operator-(const Complex& a, const Complex& b);
+{
+}
+Complex operator*(const Complex& a, const Complex& b);
+{
+}
+Complex operator/(const Complex& a, const Complex& b);
+{
+}
+
+// norm returns the squared magnitude of z
+
+double norm(const Complex& z)
+{
+}
+
+// conj returns the complex conjugate of z
+
+Complex conj(const Complex& z)
+{
+}
+
+// Comparison
+
+bool operator==(const Complex& a, const Complex& b)
+{
+}
+bool operator==(const Complex& a, double r)
+{
+}
+bool operator!=(const Complex& a, const Complex& b)
+{
+}
+bool operator!=(const Complex& a, double r)
+{
+}
+
+//write the complex number z to the output stream in the format "6+5i" or "6-5i"
+
+std::ostream& operator<<(std::ostream& out, const Complex& z)
+{
+}
