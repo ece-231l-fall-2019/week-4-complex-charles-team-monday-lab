c00e604d9b8eebc0488f9e31e4b27155bac190b9
Rizk92 <rizke@unm.edu>
2019-09-18 16:17:55 -0600

    Trail Test: Using '0' Built-In

--- runtests.cpp
+++ runtests.cpp
@@ -3,7 +3,7 @@
 // change the value below to 1 to run tests against your Complex class.
 // change the value below to 0 to run tests against the built in std::complex.
 
-#if 0
+#if 0 // TESTING Built-In complex
 #include "Complex.h"
 #else
 #include <complex>
