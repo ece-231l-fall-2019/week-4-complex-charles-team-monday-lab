64e9996a730c4093479f6cb11ff4a1bd5f03ac0d
Rizk92 <rizke@unm.edu>
2019-09-23 16:45:43 -0600

    finished rest of operators

--- Complex.cpp
+++ Complex.cpp
@@ -14,40 +14,65 @@ Complex::Complex(double re, double im)
 
 double Complex::real() const{return _real;}
 double Complex::imag() const{return _imag;}
-double rel = 0.0, img = 0.0;
+
+double rel = 0.0, img = 0.0; // Return Values
+
 Complex::Complex(const Complex& z)
 {
 	_real = z._real;
 	_imag = z._imag;
 }
+//---- Find Conjugate -----------------------
+Complex conj(const Complex& z) 
+{
+	rel = z.real();
+	img = z.imag() * -1;
+	Complex calc(rel, img);
+	return calc;
+}
+//---- Find Normal -----------------------
+double norm(const Complex& z)
+{
+	double res = 0.0;
+	rel = z.real() * z.real();
+	img = z.imag() * z.imag();
+	res = rel + img;
+	return res;
+}
+//---- cout stream << ------------------------------------------------------
 std::ostream& operator<<(std::ostream& out, const Complex& z)
 {
-	out << "(" << z.real() << ", " << z.imag() << "i)";
+	if( z.imag() > 0)
+	{
+		out << "(" << z.real() << " + " << z.imag() << "i)";
 	return out;
+	}
+	else
+	{
+		out << "(" << z.real() << " - " << z.imag() * -1 << "i)";
+		return out;
+	}
 }
-
+//---- Additon --------------------------------------------------------------
 Complex operator+(const Complex& a, const Complex& b)
 {
-	// Additon
 	rel = a.real() + b.real();
 	img = a.imag() + b.imag();
 	Complex calc(rel, img);
 	return calc;
 }
-
+//---- Subtraction ---------------------------------------------------------
 Complex operator-(const Complex& a, const Complex& b)
 {
-	// Subtraction
 	rel = a.real() - b.real();
 	img = a.imag() - b.imag();
 	Complex calc (rel, img);
-	return calc;
-	
+	return calc;	
 }
+//---- Multiplication ------------------------------------------------------
 Complex operator*(const Complex& a, const Complex& b)
 {    
-	// FOIL
-	double f = 0.0, o = 0.0, i = 0.0, l = 0.0; 
+	double f = 0.0, o = 0.0, i = 0.0, l = 0.0; // FOIL 
 	//  (Ax + Bi)(Cx - Di) 
 	f = a.real() * b.real();    //  Ax * Cx
 	o = a.real() * b.imag();    //  Ax * Di
@@ -59,9 +84,9 @@ Complex operator*(const Complex& a, const Complex& b)
 	Complex calc (rel, img);
 	return calc;
 }
+//---- Division --------------------------------------------------------
 Complex operator/(const Complex& a, const Complex& b)
 {
-	// Divsion
 	double numerX = 0.0, numerI = 0.0;              // Numerator 
 	double denom = 0.0;                             // Denominator
 	double conjX = b.real(), conjI = b.imag() * -1; // Find Conjugate
@@ -78,10 +103,70 @@ Complex operator/(const Complex& a, const Complex& b)
 	Complex calc (rel, img);
 	return calc;	
 }
-/*
-bool operator==(const Complex& a, const Complex& b);
-bool operator==(const Complex& a, double r);
-bool operator!=(const Complex& a, const Complex& b);
-bool operator!=(const Complex& a, double r);
-*/
-
+//---- Assign ---------------------------------------
+Complex& Complex::operator=(double r)
+{
+	_real = r;
+	_imag = 0.0;
+	return *this;
+}
+//---- Add Assign ------------------------------------------
+Complex& Complex::operator+=(const Complex& z)
+{
+	_real = _real + z.real();
+	_imag = _imag + z.imag();
+	return *this;
+}
+//---- Subtract Assign -------------------------------------
+Complex& Complex::operator-=(const Complex& z)
+{
+	_real = _real - z.real();
+	_imag = _imag - z.imag();
+	return *this;
+}
+//---- Multiply Assign ------------------------------------
+Complex& Complex::operator*=(const Complex& z)
+{
+	double temp = _real; // need to save original vaule of _real.
+	_real = (_real * z.real()) + ((_imag * z.imag())* -1); // becuase _real changes here
+	_imag = (temp * z.imag()) + (_imag * z.real()) ;
+	return *this;
+}
+//--- Divide Assign ---------------------------------------
+Complex& Complex::operator/=(const Complex& z)
+{
+	double temp = _real;
+	double denominator = norm(z);
+	_real = ((_real * z.real()) + (_imag * z.imag())) / denominator;
+	_imag = ((temp * -z.imag()) + (_imag * z.real())) / denominator;
+	return *this;
+}
+//----Equivalent Comparison------------------------------------------------
+bool operator==(const Complex& a, const Complex& b)
+{
+	if( a.real() == b.real() && a.imag() == b.imag())
+		return true;
+	return false;
+}
+bool operator==(const Complex& a, double r)
+{
+	if(a.real() == r && a.imag() == 0.0)
+		return true;
+	return false;
+}
+//---- Not Equivalent Complex Comparison -------------------------------------------
+bool operator!=(const Complex& a, const Complex& b)
+{
+	if(a.real() == b.real())
+		return false; 
+	else if(a.imag() == b.real())
+		return false;
+	return true;
+}
+bool operator!=(const Complex& a, double r)
+{
+	if (a.real() != r && a.imag() != 0.0)
+		return true;
+	return false;
+}
+//---- END of Program ------------------------------------------------------------------------
