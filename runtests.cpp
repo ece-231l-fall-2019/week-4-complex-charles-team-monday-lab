1c0238fb9348a0310bcb94c717acf10ba624e3c3
Rizk92 <rizke@unm.edu>
2019-09-23 15:17:04 -0600

    Add new Tests

--- runtests.cpp
+++ runtests.cpp
@@ -3,7 +3,7 @@
 // change the value below to 1 to run tests against your Complex class.
 // change the value below to 0 to run tests against the built in std::complex.
 
-#if 0 // TESTING Built-In complex
+#if 1  // TESTING Built-In complex
 #include "Complex.h"
 #else
 #include <complex>
@@ -20,35 +20,62 @@ void Assert(bool cond, std::string message)
 
 int main()
 {
+	
 //--- Test #1 ----------------------------------------------
+
 	Complex z1(4, 3), z2(5,2);
-	Complex z3(-2,-1), z4(0,0);
+	Complex z3(-1,-2), z4(-2,-4);
 	
 	std::cout <<" z1 = "<< z1 <<std::endl;
 	std::cout <<" z2 = "<< z2 <<std::endl;
-	std::cout <<" z1 + z2 = " << z1 + z2 <<std::endl; // ANSWER: (9,5)
-	std::cout <<" z1 - z2 = " << z1 - z2 <<std::endl; // ANSWER: (-1,1)
-	std::cout <<" z1 / z2 = " << z1 / z2 <<std::endl; // ANSWER: (0.896552,0.241379)
-	std::cout <<" z1 * z2 = " << z1 * z2 <<std::endl; // ANSWER: (14,23)
-	Assert(z1.real() == 4, "real constructor");
-	Assert(z3.real() == 4, "real constructor"); 
-	Assert(z3.imag() == -1, "real constructor");
-	Assert(norm(z1) == 25, "complex norm");
+	// Basic operations
+	std::cout <<" z1 + z2 = " << z1 + z2 <<std::endl; // ANSWER: (9 + 5i)
+	std::cout <<" z1 - z2 = " << z1 - z2 <<std::endl; // ANSWER: (-1 + 1i)
+	std::cout <<" z1 / z2 = " << z1 / z2 <<std::endl; // ANSWER: (0.896552 + 0.241379i)
+	std::cout <<" z1 * z2 = " << z1 * z2 <<std::endl; // ANSWER: (14 + 23i)
+
+	std::cout << "The Conjugate of z1 = " << conj(z1) << std::endl; 
+	std::cout << "The Normal of z2 = " << norm(z2) << std::endl;
 	
-	std::cout << "Does z1 == 24 : ";
-	if(z1 == z4)
-		std::cout << "yes";
+	
+	// Compare operations
+	std::cout << "Does z1 == 24 : "; //ANWER NO
+	if(z1 == z2)
+		std::cout << "yes ";
 	else
-		std::cout << "no";
+		std::cout << "no ";
 	std::cout << "\n";
 	
-//	std::cout <<" z1 += z2 " << z1 += z2;
-//	std::cout <<" : z1 = " << z1 << std::endl;
+	std::cout << "is z3 != 24 : "; // ANSWER Yes
+	if(z1 != z4)
+		std::cout << "yes";
+	else
+		std::cout << "no"; 
+	std::cout << "\n";
+	
+	
+	std::cout <<" z4 = "<< z4 <<std::endl; //ANSWER(-2 + 4i)
+	
+	std::cout << "z3 - z4  = " << z3 - z4 << std::endl; //ANSWER: (1 + 2i)
+	
+	z3 += z4; 
+	std::cout << "z3 += z4 =" << z3 << std::endl; // ANSWER (-3 - 6i)
+	
+	z3 -= z4;
+	std::cout << "z3 -= z4 =" << z3 << std::endl; // ANSWER: (-1 - 2i)
+	
+	z1 *= z2;
+	std::cout << "z1 *= z2 =" << z1 << std::endl; //ANSWER: (14 + 23i)
+	
+	z2 /= z1;
+	std::cout << "z2 /= z1 =" << z2 << std::endl; // ANSWER: (.16 - .12i)
+	
+	
+	//std::cout << "z3 -= z4 =" << z3 -= z4 << std::endl;
+	//std::cout << "z3 *= z4 =" << z3 *= z4 << std::endl;
+	//std::cout << "z3 /= z4 =" << z3 /= z4 << std::endl;
+	
 /*
-	Assert(z1.real() == 4, "real constructor");
-	Assert(z1.imag() == 3, "real constructor");
-	Assert(norm(z1) == 25, "complex norm");
-
 //--- Test #2 ---------------------------------------------
 	Complex z4 = z1 - z2;
 	std::cout << "z4 = " << z4 << "\n z3 = \n ";
