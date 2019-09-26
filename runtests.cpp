98d4524409aa5109e91a5b0aa4dc5a738654a1af
Rizk92 <rizke@unm.edu>
2019-09-26 14:49:49 -0600

    Completed Final Testing

--- runtests.cpp
+++ runtests.cpp
@@ -31,7 +31,7 @@ int main()
 	Assert(z1.imag() == 3, "real constructor");
 //--- Test #2 ---- Assert Complex Z4 ---------------------------------------
 	Assert(z4.real() == -2, "real constructor");
-	Assert(z4.imag() == -4, "real constructor");
+	Assert(z4.imag() == 4, "real constructor");
 //--- Test #3 ---- Assert Complex Z6 ---------------------------------------
 	Assert(z6.real() == 6, "real constructor");
 	Assert(z6.imag() == 0, "real constructor");
